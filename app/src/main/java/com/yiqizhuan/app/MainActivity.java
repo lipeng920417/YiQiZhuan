1744faeda16709137346ee9e816cddbfba0d80c3
lipeng <lipeng@auto-link.com.cn>
2024-05-30 15:16:00 +0800

    feat:修改底部样式

--- app/src/main/java/com/yiqizhuan/app/MainActivity.java
+++ app/src/main/java/com/yiqizhuan/app/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
